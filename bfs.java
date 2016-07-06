0c0995ce92d06359afd35a24e8d45ae4931d768e
Yedle Sachin Bharat <yedlesachin60@gmail.com>
2016-07-06 10:42:41 +0530

    Update bfs.java

--- bfs.java
+++ bfs.java
@@ -3,30 +3,38 @@ import java.util.*;
 
 public class Solution {
 
-    
-    private static Scanner in = new Scanner(System.in);
+    private int N;    
     private static LinkedList<Integer> []adj;
-    private static void initialize(int n,int m){        
+    Solution(int n){
+        N=n;        
         adj=new LinkedList[n+1];
-        for(int j=0;j<n;j++)
+        for(int j=0;j<n+1;j++)
             adj[j]=new LinkedList();
     }
-    private static void bfs(int s,int n){
-        int x=0;        
-        boolean []visited=new boolean[n+1];
-        int []cost=new int[n+1];
-        Arrays.fill(cost,-1);
+     
+    void addEdge(int a,int b){
+        adj[a].add(b);
+        adj[b].add(a);
+        
+    }
+   
+    void bfs(int s){
+        int x=6;    
+        int b=s;
+        boolean []visited=new boolean[N+1];
+        int []cost=new int[N+1];        
         LinkedList<Integer> queue = new LinkedList<Integer>();
-        visited[s]=true;
-        queue.add(s);
+        visited[s]=true;        
+        queue.add(s);        
         while(queue.size()!=0){
             s=queue.poll();
-            x=x+6;
-            cost[s]=x;
+            int a=cost[s]+6;
             Iterator<Integer> itr=null; 
             try{
                 itr = adj[s].listIterator();            
-            }           
+            }catch(Exception e){
+                System.out.println("Exception:"+e);
+            }        
             while (itr.hasNext())
             {
                 int k = itr.next();
@@ -34,27 +42,37 @@ public class Solution {
                 {
                     visited[k] = true;
                     queue.add(k);
-                    cost[k]=x;
+                    cost[k]=a;
                 }
             }
-        }       
-        System.out.println();
+        }   
+        String str="";
+        for(int i=1;i<cost.length;i++){
+            if(i==b)
+                continue;
+            else{
+                cost[i]=(cost[i]==0?-1:cost[i]);
+                str+=cost[i]+" ";                
+            }
+                
+        }
+        System.out.println(str);
     }    
     public static void main(String[] args) {        
+        Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        int n = 0, m = 0, s=0;
-        int x = 0, y = 0;        
+        int n = 0, m = 0, s=0;               
         for(int i = 0 ; i < t ; i++){
             n = in.nextInt();
-            m = in.nextInt();
-            initialize(n,m);
+            Solution sl=new Solution(n);
+            m = in.nextInt();            
             for(int j=0;j<m;j++){
                 int a=in.nextInt();
                 int b=in.nextInt();
-                adj[a].add(b);
+                sl.addEdge(a,b);
             }
             s=in.nextInt();
-            bfs(s,n);
+            sl.bfs(s);
         }
     }
 }
